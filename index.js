0d4cefc594f6b00b4a4f70a41f56266882dc794d
kurttheviking <kurt@agilediagnosis.com>
2014-08-26 23:46:44 -0700

    add common 4xx errors: unauthorized, forbidden, conflict, gone

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var typedError = require('typederror');
 
-module.exports.NotFound = typedError('NotFoundError', {code: 404});
 module.exports.Invalid = typedError('InvalidError', {code: 400});
+module.exports.Unauthorized = typedError('UnauthorizedError', {code: 401});
+module.exports.Forbidden = typedError('ForbiddenError', {code: 403});
+module.exports.NotFound = typedError('NotFoundError', {code: 404});
+module.exports.Conflict = typedError('ConflictError', {code: 409});
+module.exports.Gone = typedError('GoneError', {code: 410});
 module.exports.Err = typedError('Error', {code: 500});
 
 // for back-compat
