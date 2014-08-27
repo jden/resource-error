abb0ecf0c33a5c73e756c649acbb5d157ece3c8c
kurttheviking <kurt@agilediagnosis.com>
2014-08-26 23:44:19 -0700

    provide backcompat for NotAuthorized

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var typedError = require('typederror');
 
 module.exports.NotFound = typedError('NotFoundError', {code: 404});
-module.exports.NotAuthorized = typedError('NotAuthorizedError', {code: 403});
 module.exports.Invalid = typedError('InvalidError', {code: 400});
 module.exports.Err = typedError('Error', {code: 500});
+
+// for back-compat
+module.exports.NotAuthorized = typedError('NotAuthorizedError', {code: 403});
